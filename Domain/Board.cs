f7bb96a6bc71c0ae3495ebcba3af8c847f3e7037
БражникД <@223-2>
2023-07-14 16:23:54 +0600

    Добавлено отображение ячеек в цикле; Добавлен метод нажатия по ячейке; Добавлена логика смена хода.

--- Domain/Board.cs
+++ Domain/Board.cs
@@ -4,7 +4,15 @@
     {
         public int ColumnCount => 3;
         public int RowCount => 3;
-        public CellState Cell_0_0 { get; set; } = CellState.X;
+
+        public CellState[,] Cells { get; set; }
+
+        public Board()
+		{
+            Cells = new CellState[RowCount, ColumnCount];
+		}
+
+        public CellState Cell_0_0 { get; set; }
         public CellState Cell_0_1 { get; set; }
         public CellState Cell_0_2 { get; set; }
         public CellState Cell_1_0 { get; set; }
@@ -13,6 +21,23 @@
         public CellState Cell_2_0 { get; set; }
         public CellState Cell_2_1 { get; set; }
         public CellState Cell_2_2 { get; set; }
+        
+        public CellState CurrentTurn = CellState.X;
+
+        public void SwitchTurn()
+        {
+            CurrentTurn = CurrentTurn == CellState.X ? CellState.O : CellState.X;
+        }
+
+        public void CellClick(int row, int column)
+        {
+            if (Cells[row, column] == CellState.Blank)
+            {
+                Cells[row, column] = CurrentTurn;
+
+                SwitchTurn();
+            }
+        }
     }
 
     public enum CellState

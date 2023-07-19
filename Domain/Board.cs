809283c28c78797487f3650a91cc18454b17dd7e
БражникД <@223-2>
2023-07-19 18:12:57 +0600

    Добавлено отображение победы: Добавлена остановка ход при победе или нечьей.

--- Domain/Board.cs
+++ Domain/Board.cs
@@ -21,31 +21,36 @@
 
         public void CellClickWithTurn(int row, int column)
         {
-            if (Cells[row, column] == CellState.Blank)
+            var gameResult = GetGameResult(out _);
+            if(gameResult == GameResult.Unknown)
             {
-                Cells[row, column] = CurrentTurn;
+                if (Cells[row, column] == CellState.Blank)
+                {
+                    Cells[row, column] = CurrentTurn;
 
-                SwitchTurn();
+                    SwitchTurn();
+                }
             }
+            
         }
 
-        public GameResult GetGameResult()
+        public GameResult GetGameResult(out CellPosition[] winCells)
         {
-            if (CheckWin(Gamer.X))
+            if (CheckWin(Gamer.X, out winCells))
             {
                 return GameResult.WonX;
             }
-            else if (CheckWin(Gamer.O))
+            else if (CheckWin(Gamer.O, out winCells))
             {
                 return GameResult.WonO;
             }
             else
             {
-                return GameResult.Draw;
+                return GameResult.Unknown;
             }
         }
 
-        public bool CheckWin(Gamer gamer)
+        public bool CheckWin(Gamer gamer, out CellPosition[] winCells)
         {
             CellState expectedCell;
 
@@ -66,13 +71,44 @@
                 }
                 if(count == 3)
                 {
+                    winCells = new CellPosition[]
+                    {
+                        new CellPosition(row: i, column: 0),
+                        new CellPosition(row: i, column: 1),
+                        new CellPosition(row: i, column: 2),
+                    };
                     return true;
                 }
             }
 
+            for (int j = 0; j < ColumnCount; j++)
+            {
+                var count = 0;
+                for (int i = 0; i < RowCount; i++)
+                {
+                    if (Cells[i, j] == expectedCell)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 3)
+                {
+                    winCells = new CellPosition[]
+                    {
+                        new CellPosition(row: 0, column: j),
+                        new CellPosition(row: 1, column: j),
+                        new CellPosition(row: 2, column: j)
+                    };
+                    return true;
+                }
+            }
+
+            winCells = new CellPosition[0];
             return false;
         }
     }
+
+    public record CellPosition(int row, int column);
 
     public enum CellState
     {
@@ -83,7 +119,10 @@
 
     public enum GameResult
     {
-        WonX, WonO, Draw
+        WonX, 
+        WonO,
+        Unknown,
+        Draw
     }
 
     public enum Gamer
